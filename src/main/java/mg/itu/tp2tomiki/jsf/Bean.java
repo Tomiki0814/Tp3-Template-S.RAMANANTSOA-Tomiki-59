defb4a2d9bbd766c1ab1ac44e38b12aec6d2ecfd
Tomiki0814 <tomikiramanantsoa@gmail.com>
2023-04-02 12:15:36 +0300

    init from tp2

---
+++ src/main/java/mg/itu/tp2tomiki/jsf/Bean.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tp2tomiki.jsf;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author tomik
+ */
+@Named(value = "bean")
+@RequestScoped
+public class Bean {
+
+    /**
+     * Creates a new instance of Bean
+     */
+    public Bean() {
+    }
+    private int nombre;
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+
+    public String afficher() {
+        return "affichage_3?nb=" + nombre + "&;faces-redirect=true";
+    }
+
+}
